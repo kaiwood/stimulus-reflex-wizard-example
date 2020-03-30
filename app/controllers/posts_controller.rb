e6662cc9d35ed8c7cbb24693c72f64088c3e2c34
Kai Wood <kwood@kwd.io>
2020-03-30 12:25:31 +0200

    Add create / show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     @post ||= Post.persistant(session.id)
     @step ||= Rails.cache.fetch("step:#{session.id}") { :title }
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
